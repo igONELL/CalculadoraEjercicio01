8f8bdd7969c6ad0188ffd64d345dccc411ce1916
igONELL <igorllancapan@gmail.com>
2020-04-16 04:39:13 -0400

    Calculadora con fines educativos ejercicion 01

---
+++ Aritmetica.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculadora
+{
+    class Aritmetica
+    {
+        public int Suma(int a, int b)
+        {
+
+            return a + b;
+
+        }
+
+        public int Resta(int a, int b)
+        {
+
+            return a - b;
+
+        }
+
+        public int Multi(int a, int b)
+        {
+
+            return a * b;
+
+        }
+
+        public int Divi(double tmpDividendo, double tmpDivisor)
+        {
+
+            int divisor = int.Parse(Math.Round(tmpDivisor).ToString());
+            int dividendo = int.Parse(Math.Round(tmpDividendo).ToString());
+            divisor = Math.Abs(divisor);
+            Console.WriteLine("La division por 0 no es valida.");
+            if (divisor == 0)
+
+                return 0;
+
+            return dividendo / divisor;
+
+
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        private static Aritmetica _aritmetica = new Aritmetica();
+
+        static void Main(string[] args)
+        {
+            /*Menu*/
+
+            Console.WriteLine("Bienvenido a la Calculadora:");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+            Console.WriteLine("3. Multiplicar");
+            Console.WriteLine("4. Dividir");
+            Console.WriteLine("0. Salir");
+            string input = Console.ReadLine().ToLower().Trim();
+            try
+            {
+                int intInput = int.Parse(input);
+                int a = 0, b = 0;
+
+                switch (intInput) //Se utiliza switch para ir leyendo y detenerse cuando encuentre el resultado
+                {
+                    case 1:
+                        Console.WriteLine("Ingrese el primer factor:");
+                        a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo factor:");
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + _aritmetica.Suma(a, b));
+                        break;
+                    case 2:
+                        Console.WriteLine("Ingrese el primer digito:");
+                        a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo digito:");
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + _aritmetica.Resta(a, b));
+                        break;
+                    case 3:
+                        Console.WriteLine("Ingrese el primer digito:");
+                        a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo digito:");
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + _aritmetica.Multi(a, b));
+                        break;
+                    case 4:
+                        Console.WriteLine("Ingrese el primer digito:");
+                        a = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo digito:");
+                        b = int.Parse(Console.ReadLine());
+                        Console.WriteLine("El resultado es: " + _aritmetica.Divi(a, b));
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("No se puede leer el input porque no es un numero:");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
